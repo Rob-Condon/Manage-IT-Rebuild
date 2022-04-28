9f106e5c55350e8462aef924a8093a3b9b148fb6
Rob Condon <rtc33@uclive.ac.nz>
2022-04-28 21:03:13 +1200

    Bussiness images bar.

--- src/components/home/infomation/BusinessStrip.vue
+++ src/components/home/infomation/BusinessStrip.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    <img src="src/assets/ClientLogos/WindowTreatments.jpg" alt="widnow treatments logo">
-    <img src="src/assets/ClientLogos/WildBerries.jpg" alt="widnow treatments logo">
-    <img src="src/assets/ClientLogos/ShantyTown.jpg" alt="widnow treatments logo">
-    <img src="src/assets/ClientLogos/FoxAndAssociates.jpg" alt="widnow treatments logo">
+  <div class="flex justify-between content-between ph6 pv2" style="align-items: center" >
+    <img :src='require("@/assets/ClientLogos/WindowTreatments.jpg")' class="no-flex"  alt="widnow treatments logo">
+    <img :src='require("@/assets/ClientLogos/WildBerries.jpg")' class="no-flex" alt="wild berries logo">
+    <img :src='require("@/assets/ClientLogos/ShantyTown.jpg")' class="no-flex" alt="shanty town logo">
+    <img :src='require("@/assets/ClientLogos/FoxAndAssociates.jpg")' class="no-flex" alt="fox and associates logo">
+    <img :src='require("@/assets/ClientLogos/KannangaraThomson.jpg")' class="no-flex" alt="Kannangara Thomson logo">
+    <img :src='require("@/assets/ClientLogos/FurnitureSolutionsLogo.png")' class="no-flex" alt="Furniture Solutions Logo">
   </div>
 </template>
 
@@ -14,5 +16,7 @@ export default {
 </script>
 
 <style scoped>
-
+.no-flex {
+  flex: initial;
+}
 </style>
